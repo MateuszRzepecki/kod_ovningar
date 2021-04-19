c8b6f441f1afece7aee33a80f04fb5ab77ed08ec
MateuszRzepecki <71072305+MateuszRzepecki@users.noreply.github.com>
2021-04-19 19:50:23 +0200

    Update
    eureka

--- prototyp.py
+++ prototyp.py
@@ -1,6 +1,13 @@
-def sök_funktion():
-    f = open("stats.txt", "rt")
-    Fråga = input("Vad söker du efter?:")
-    
+def search_function():
+     # Definierar funktionen
+    txt = open("stats.txt", "rt")
+    # Öppnar text-filen
+    question = input("Vad söker du efter?:")
+    # Skapar en input som frågar användaren om det de söker
+    lines = txt.readlines()
+    for line in lines:
+        if question in line:
+            print(line.strip()[-2:])
+            break
 
-sök_funktion()
+search_function()
