0f355bf8d896871cc0c0f1414d5502e0044870ea
MateuszRzepecki <tishel19matrze@innovationsgymnasiet.se>
2021-04-21 14:22:00 +0200

    Update uppgift_diceroll.py

--- uppgift_diceroll.py
+++ uppgift_diceroll.py
@@ -1,7 +1,8 @@
+import random
+# Importerar random modulen så att den kan användas
 def diceroll_function():
+    result = []
 # Definierar funktionen
-    import random
-    # Importerar random modulen så att den kan användas
     for x in range(1000):
         print(random.randint(1, 6))
         # Loop som körs 1000 gånger, printar ett slumpmässigt nummer mellan 1 till 6  
