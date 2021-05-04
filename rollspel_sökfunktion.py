4261ce944e18077c8ea440fbebd89a7077909341
MateuszRzepecki <tishel19matrze@innovationsgymnasiet.se>
2021-05-04 12:54:08 +0200

    Update rollspel_sökfunktion.py

--- rollspel_sökfunktion.py
+++ rollspel_sökfunktion.py
@@ -1,19 +1,15 @@
 def search_function():
     # Definierar funktionen
     txt = open("stats.txt", "rt")
-    # Öppnar text-filen 
+# Öppnar text-filen
     question = input("Vad söker du efter?:")
-    # Skapar en input som frågar användaren om det de söker
+# Skapar en input som frågar användaren om det de söker
     lines = txt.readlines()
     for line in lines:
         if question in line:
             print(line.strip())
             txt.close()
             break
-        if question not in line:
-            print(str(question) + " " + "saknas.")
-            txt.close()
-            break
-# Loopen letar efter användarens input i textfilen och om den hittas så printas den 
+# Loopen letar efter användarens input i textfilen och om den hittas så printas den
 search_function()
 # Startar funktionen
