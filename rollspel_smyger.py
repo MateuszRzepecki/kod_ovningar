6bc969f392dfce2ff9948d3e5d8ee2d80d8452bf
MateuszRzepecki <tishel19matrze@innovationsgymnasiet.se>
2021-05-05 20:05:53 +0200

    5
    6

--- rollspel_smyger.py
+++ rollspel_smyger.py
@@ -7,7 +7,7 @@ def dice_roll():
     # Öppnar textfilen
     dice_sides = input("How many sides does your dice have?:")
     # Låter användaren välja den maxiala antal sidor som tärningen har
-    question = input("Compare?:")
+    question = input("What stat are you looking for?:")
     # Här bestäms vilken stat som ska gemföras med tärningen
     l = len(question)
     roll_20 = random.randint(1, int(dice_sides)) 
@@ -16,7 +16,7 @@ def dice_roll():
     for line in lines:
         if question in line:
             print("Your stat:" + line[l+1:100])
-            # Om det man söker finns så printas talet 
+            # En loop genom texten, om attributen/färdigheten man söker finns så printas talet för den
     print("You rolled:" + " " + str(roll_20))
     #if roll_20 >= int(line[l+1:100]):
         #print("You did it!")
@@ -25,7 +25,7 @@ def dice_roll():
     if roll_20 == int(1):
         print("Your action was executed perfectly!")
     if roll_20 == dice_sides:
-        print("Your action ")
+        print("Your action almost failed...")
 
         
  
