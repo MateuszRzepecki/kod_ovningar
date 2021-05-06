fd503a26833ba6bf197eb7506bf74813f450cd3c
MateuszRzepecki <tishel19matrze@innovationsgymnasiet.se>
2021-05-06 11:16:18 +0200

    55
    jbjhikb

--- rollspel_smyger.py
+++ rollspel_smyger.py
@@ -10,22 +10,21 @@ def dice_roll():
     question = input("What stat are you looking for?:")
     # Här bestäms vilken stat som ska gemföras med tärningen
     l = len(question)
-    roll_20 = random.randint(1, int(dice_sides))
+    roll = random.randint(1, int(dice_sides))
     lines = txt.readlines() 
     for line in lines:
         if question in line:
-            print("Your stat:" + line[l+1:100])
+            roll_result = line[l+1:100]
+            print("Your stat:" + str(roll_result))
             # En loop genom texten, om attributen/färdigheten man söker finns så printas talet för den
-    q = []
-    t = q.extend(line[l+1:100])
-    print("You rolled:" + " " + str(roll_20))
-    #if roll_20 >= int(q):
-        #print("You did it!")
-    #if roll_20 < line[l+1:100]:
-        #print("You failed.")
-    if roll_20 == int(1):
+    print("You rolled:" + " " + str(roll))
+    if roll >= int(roll_result):
+        print("You did it!")
+    if roll < int(roll_result):
+        print("You failed.")
+    if roll == 1:
         print("Your action was executed perfectly!")
-    if roll_20 == dice_sides:
+    if roll == dice_sides:
         print("Your action almost failed...")
         
  
