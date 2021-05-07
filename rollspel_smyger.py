8efbfabcc97cbc612039fff1aa1e11ce6cb50c8b
MateuszRzepecki <71072305+MateuszRzepecki@users.noreply.github.com>
2021-05-07 16:03:42 +0200

    comment update
    new comments added and re-named dice_sides variable to dice_max

--- rollspel_smyger.py
+++ rollspel_smyger.py
@@ -1,30 +1,33 @@
 import random
-# Importerar en modulen random 
+# Importerar modulen random 
 
 def dice_roll():
 # Definierar funktion
     txt = open("stats.txt", "rt")
     # Öppnar textfilen
-    dice_sides = input("How many sides does your dice have?:")
-    # Låter användaren välja den maxiala antal sidor som tärningen har
+    dice_max = input("How many sides does your dice have?:")
+    # Låter användaren välja det maximala antal sidor som tärningen har
     question = input("What stat are you looking for?:")
-    # Här bestäms vilken stat som ska gemföras med tärningen
+    # Här bestäms vilken attribut/färdighet som ska gemföras med tärningens slag
     l = len(question)
-    roll = random.randint(1, int(dice_sides))
-    lines = txt.readlines() 
+    roll = random.randint(1, int(dice_max))
+    # Variabel som fungerar som tärningen
+    lines = txt.readlines()
+    # Läser textfilen
     for line in lines:
         if question in line:
             roll_result = line[l+1:100]
             print("Your stat:" + str(roll_result))
-            # En loop genom texten, om attributen/färdigheten man söker finns så printas talet för den
+            # En loop genom texten, om attributen/färdigheten man söker finns så printas talet
     print("You rolled:" + " " + str(roll))
+    # printar ut talet man har fått från slaget
     if roll >= int(roll_result):
         print("You did it!")
     if roll < int(roll_result):
         print("You failed.")
     if roll == 1:
         print("Your action was executed perfectly!")
-    if roll == dice_sides:
+    if roll == dice_max:
         print("Your action almost failed...")
         
  
