abf065ea41d50dbaedf38f4ed6ff566a4abff2cb
MateuszRzepecki <71072305+MateuszRzepecki@users.noreply.github.com>
2021-05-07 16:40:56 +0200

    final update for now
    Done with the current version, moving on I will ork on ObT6

--- rollspel_smyger.py
+++ rollspel_smyger.py
@@ -7,28 +7,30 @@ def dice_roll():
     # Öppnar textfilen
     dice_max = input("How many sides does your dice have?:")
     # Låter användaren välja det maximala antal sidor som tärningen har
-    question = input("What stat are you looking for?:")
+    stat_q = input("What stat are you looking for?:")
     # Här bestäms vilken attribut/färdighet som ska gemföras med tärningens slag
-    l = len(question)
+    l = len(stat_q)
+    # 
     roll = random.randint(1, int(dice_max))
     # Variabel som fungerar som tärningen
     lines = txt.readlines()
     # Läser textfilen
     for line in lines:
-        if question in line:
-            roll_result = line[l+1:100]
-            print("Your stat:" + str(roll_result))
+        if stat_q in line:
+            stat_comp = line[l+1:100]
+            print("Your stat:" + str(stat_comp))
             # En loop genom texten, om attributen/färdigheten man söker finns så printas talet
-    print("You rolled:" + " " + str(roll))
-    # printar ut talet man har fått från slaget
-    if roll >= int(roll_result):
+            print("You rolled:" + " " + str(roll))
+            # printar ut talet man har fått från slaget
+    if roll > int(stat_comp):
         print("You did it!")
-    if roll < int(roll_result):
+    if roll < int(stat_comp):
         print("You failed.")
-    if roll == 1:
+    if int(stat_comp) == 1:
         print("Your action was executed perfectly!")
-    if roll == dice_max:
+    if int(stat_comp) == int(dice_max):
         print("Your action almost failed...")
+    # If satser som syftar på att beskriva olika fall där värdet från kastet och attribut/färdighet gemförs
         
  
     txt.close()
