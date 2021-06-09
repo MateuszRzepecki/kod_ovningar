ea643d63a66f0d3c02a37521636d85c00541940d
MateuszRzepecki <71072305+MateuszRzepecki@users.noreply.github.com>
2021-06-09 14:26:19 +0200

    final update
    finished GetSkill() function

--- GetSkill_komplettering.py
+++ GetSkill_komplettering.py
@@ -12,11 +12,13 @@ def GetSkill():
             "value": 14    
         }
     }
-    #
+    # Frågar användaren om skillen vilkens värde ska hittas
     request = input("Which skill value are you looking for?: ")
+    # For-loop kör genom nested dictionary
     for x in skill_dict:
+        # Om det användaren söker hittas i nested dictionary printar den värdet för skillen
         if request in skill_dict: 
-            skill_value = skill_dict.get("value")
-            print("Skill value " + str(skill_value))
+            skill_value = skill_dict.get(str(request))
+            print("Skill value: " + str(skill_value))
             break
 GetSkill()
