094a2e983f005da1b661fff55752f2dbbffd2d6c
MateuszRzepecki <71072305+MateuszRzepecki@users.noreply.github.com>
2021-11-20 18:45:28 +0100

    Update uppgift_räkna_bokstav.py

--- uppgift_räkna_bokstav.py
+++ uppgift_räkna_bokstav.py
@@ -1,11 +1,11 @@
 target = input('Ange en bokstav:')
 text = input('Skriv text:')
-antal = 0
+amount = 0
 
 for i in text:
     if i == target:
-        antal = antal + 1
-print('Bokstaven ' + str(target) + ' finns ' + str(antal) + ' gånger i texten.')
+        amount = amount + 1
+print('Bokstaven ' + str(target) + ' finns ' + str(amount) + ' gånger i texten.')
 
 
 
