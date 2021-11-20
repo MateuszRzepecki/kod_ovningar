c649f94f6facb62c7b4877947c9577eb4bc8077c
MateuszRzepecki <71072305+MateuszRzepecki@users.noreply.github.com>
2021-11-20 18:33:42 +0100

    Update uppgift_räkna_bokstav.py

--- uppgift_räkna_bokstav.py
+++ uppgift_räkna_bokstav.py
@@ -1,10 +1,11 @@
 target = input('Ange en bokstav:')
 text = input('Skriv text:')
+antal = 0
 
-def CharacterCounter():
-    antal = 0
-    for i in text:
-        if i == target:
-            antal = antal + 1
-    print('Bokstaven ' + str(target) + ' finns ' + str(antal) + ' gånger i texten.')
-CharacterCounter()
+for i in text:
+    if i == target:
+        antal = antal + 1
+print('Bokstaven ' + str(target) + ' finns ' + str(antal) + ' gånger i texten.')
+
+
+
