9e79f6f552214702992acb2e8ad6395890adf12d
MateuszRzepecki <tishel19matrze@innovationsgymnasiet.se>
2021-12-08 10:17:26 +0100

    Update bruh.py

--- bruh.py
+++ bruh.py
@@ -1,13 +1,33 @@
 from math import * 
 
-def barn():
-    antalbarn = 0
-    n = 0
-    while n<12:
-        n = n + 1
-        antalbarn = antalbarn + 4000 + 2000 * cos(pi*n/6)
-    print (antalbarn)
-barn()
+def f(x): 
+    return 5*cos(x) - (4*x)
+
+def f_prim(x):
+    h = 0.001
+    return (f(x + h) - f(x)) / h
+
+x = 0.5
+
+while abs(f(x)) >= 0.0000001:
+    x = x - f(x)/f_prim(x)
+    print(x)
+
+print("Den sökta roten är x =", x)
+
+
+
+
+
+
+# def barn():
+    # antalbarn = 0
+    # n = 0
+    # while n<12:
+    # n = n + 1
+        # antalbarn = antalbarn + 4000 + 2000 * cos(pi*n/6)
+    # print (antalbarn)
+# barn()
 
 
 
